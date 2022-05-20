fd126157f8b7505dd8efa8a618bb4aba6a584b25
Mickael Dias <83729944+Mickael-Luiz@users.noreply.github.com>
2022-05-20 16:17:58 -0400

    Add files via upload

--- src/componentes/Pages/App.tsx
+++ src/componentes/Pages/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import Cronometro from '../Cronometro';
 import Formulario from '../Formulario';
 import Lista from '../Lista';
-import style from './style.module.scss'
+import { ITarefa } from '../types/tarefa';
+import style from './App.module.scss'
 
 function App() {
+
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa){
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAteriores => tarefasAteriores.map(tarefa => ({
+      ...tarefa ,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
+  }
+
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Cronometro />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
     </div>
   );
 }
